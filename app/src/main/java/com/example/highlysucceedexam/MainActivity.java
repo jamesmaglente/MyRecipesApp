59ae17b2594e5c410b31713275bc0e0e220df05f
thecryptoexplorer <thecryptoexplorer@protonmail.com>
2023-09-27 12:19:25 +0800

    Updated MainActivity

--- app/src/main/java/com/example/highlysucceedexam/MainActivity.java
+++ app/src/main/java/com/example/highlysucceedexam/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //changing status bar color
         changeStatusBarColor();
 
         llNetworkError = findViewById(R.id.ll_network_error);
